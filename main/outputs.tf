24f2242e9d457a6cf826c4bf9b202e7c148471b5
YalagandhulaAkhil <yalagandhula23akhil@gmail.com>
2025-06-23 17:30:14 -0400

    updated

--- main/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "lambda_role_arn" {
-  description = "The ARN of the created Lambda IAM role"
-  value       = module.lambda_iam_role.role_arn
-}
-
-output "lambda_role_name" {
-  description = "The name of the created Lambda IAM role"
-  value       = module.lambda_iam_role.role_name
-}
